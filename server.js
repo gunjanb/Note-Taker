41f671ce6b5178cc66d173f533763f2aaa417e3c
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-06-08 13:55:50 -0700

    updated port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require("path");
 const app = express();
 
 // Sets an initial port and we use this later in listener
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
